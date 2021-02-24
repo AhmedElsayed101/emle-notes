f60233a441f76472331d183d8f7a09abcbf895f3
AhmedElsayed101 <elsayedahmed945@gmail.com>
2021-02-24 14:50:25 +0200

    init commit and connect db

---
+++ config/db-config.js
@@ -0,0 +1,34 @@
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize('database', 'username', 'password', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+//   operatorsAliases: false,
+
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   },
+
+//   // SQLite only
+//   storage: 'path/to/database.sqlite'
+// });
+
+// // Or you can simply use a connection uri
+// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+
+
+module.exports = {
+  HOST: "localhost",
+  USER: "postgres",
+  PASSWORD: "0616962130",
+  DB: "emle",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
